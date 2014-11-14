7b4c4070fe04c5ffdde3b07b653af0a24d2d6ad9
bulbrain <georgi_d_g1@abv.bg>
2014-11-14 23:47:35 +0100

    added spikes

--- game.js
+++ game.js
@@ -49,9 +49,56 @@ function initCanvas() {
             birdY += 5;
         }
 */
+        upperSpikes();
+        lowerSpikes();
         ctx.restore();
     }
     var animateInterval = setInterval(animate, 15);
+    function upperSpikes() {
+        ctx.beginPath();
+        ctx.moveTo(50,0);
+        ctx.lineTo(75, 25);
+        ctx.lineTo(100, 0);
+        ctx.lineTo(125, 25);
+        ctx.lineTo(150, 0);
+        ctx.lineTo(175, 25);
+        ctx.lineTo(200, 0);
+        ctx.lineTo(225, 25);
+        ctx.lineTo(250, 0);
+        ctx.lineTo(275, 25);
+        ctx.lineTo(300, 0);
+        ctx.lineTo(325, 25);
+        ctx.lineTo(350, 0);
+        ctx.lineTo(375, 25);
+        ctx.lineTo(400, 0);
+        ctx.lineTo(425, 25);
+        ctx.lineTo(450, 0);
+        ctx.closePath();
+        ctx.stroke();
+    }
+
+    function lowerSpikes() {
+        ctx.beginPath();
+        ctx.moveTo(50,600);
+        ctx.lineTo(75, 575);
+        ctx.lineTo(100, 600);
+        ctx.lineTo(125, 575);
+        ctx.lineTo(150, 600);
+        ctx.lineTo(175, 575);
+        ctx.lineTo(200, 600);
+        ctx.lineTo(225, 575);
+        ctx.lineTo(250, 600);
+        ctx.lineTo(275, 575);
+        ctx.lineTo(300, 600);
+        ctx.lineTo(325, 575);
+        ctx.lineTo(350, 600);
+        ctx.lineTo(375, 575);
+        ctx.lineTo(400, 600);
+        ctx.lineTo(425, 575);
+        ctx.lineTo(450, 600);
+        ctx.closePath();
+        ctx.stroke();
+    }
 }
 
 window.addEventListener('load', function(event) {

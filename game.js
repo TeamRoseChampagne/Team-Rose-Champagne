f9f908fbced6005916ce2ec5313b5f90a417c479
Velichko Golev <v.golev@yahoo.com>
2014-11-16 14:44:53 +0200

    side spikes, tidied up the code

--- game.js
+++ game.js
@@ -15,16 +15,31 @@ function initCanvas() {
         ctx.save();
         ctx.clearRect(0, 0, cW, cH);
 
-        scoreCircle();
-        scorePoints();
-
-        ctx.drawImage(bird, birdX, birdY);
         //game speed controls
         var game = true;
         if (game) {
-            birdX += 2 * xDirection;
-            birdY += 3 * yDirection;
+
+            scoreCircle();
+            scorePoints();
+            birdMovement();
+            upperSpikes();
+            lowerSpikes();
+            drawLeftSpikes();
+            drawRightSpikes();
+
+            ctx.restore();
+
+            birdJump();
         }
+    }
+
+    var animateInterval = setInterval(animate, 15);
+
+    function birdMovement() {
+        ctx.drawImage(bird, birdX, birdY);
+
+        birdX += 2 * xDirection;
+        birdY += 3 * yDirection;
 
         if(birdX >= 450) {
             birdX = 450;
@@ -39,12 +54,12 @@ function initCanvas() {
             score++;
         }
         if (birdY >= 540)
-        { animate() ==false;
+        {   //animate() ==false;
             birdY = 540;
             yDirection = 0;
         }
         else if(birdY <= 0) {
-            animate() == false;
+            //animate() == false;
             birdY = 0;
             yDirection = 1;
         }
@@ -63,13 +78,9 @@ function initCanvas() {
             jump();
             maxJump = birdY - 60;
         });
+    }
 
-        upperSpikes();
-        lowerSpikes();
-
-        ctx.restore();
-
-        //begin to fall again after jump
+    function birdJump() {
         if (birdY <= maxJump) {
             yDirection = 1;
             if (xDirection > 0) {
@@ -80,8 +91,6 @@ function initCanvas() {
             }
         }
     }
-
-    var animateInterval = setInterval(animate, 15);
 
     function scoreCircle() {
         var radius = 150;
@@ -159,6 +168,51 @@ function initCanvas() {
         ctx.fillStyle='#BE2116';
         ctx.fill();
     }
+
+    var leftSpikes = [ {"x" : "0", "y" : "50", "tipX" : "25", "tipY" : "75", "endY" : "100"},
+                       {"x" : "0", "y" : "150", "tipX" : "25", "tipY" : "175", "endY" : "200"},
+                       {"x" : "0", "y" : "250", "tipX" : "25", "tipY" : "275", "endY" : "300"},
+                       {"x" : "0", "y" : "350", "tipX" : "25", "tipY" : "375", "endY" : "400"},
+                       {"x" : "0", "y" : "450", "tipX" : "25", "tipY" : "475", "endY" : "500"},
+                       {"x" : "0", "y" : "550", "tipX" : "25", "tipY" : "575", "endY" : "600"}
+    ];
+
+    var rightSpikes = [ {"x" : "500", "y" : "50", "tipX" : "475", "tipY" : "75", "endY" : "100"},
+                        {"x" : "500", "y" : "150", "tipX" : "475", "tipY" : "175", "endY" : "200"},
+                        {"x" : "500", "y" : "250", "tipX" : "475", "tipY" : "275", "endY" : "300"},
+                        {"x" : "500", "y" : "350", "tipX" : "475", "tipY" : "375", "endY" : "400"},
+                        {"x" : "500", "y" : "450", "tipX" : "475", "tipY" : "475", "endY" : "500"},
+                        {"x" : "500", "y" : "550", "tipX" : "475", "tipY" : "575", "endY" : "600"}
+    ];
+
+    function drawLeftSpikes() {
+        for (var i = 0; i < leftSpikes.length; i++) {
+            var ls = leftSpikes[i];
+            ctx.beginPath();
+            ctx.moveTo(ls.x, ls.y);
+            ctx.lineTo(ls.tipX, ls.tipY);
+            ctx.lineTo(ls.x, ls.endY);
+            ctx.closePath();
+            ctx.fillStyle='#BE2116';
+            ctx.stroke();
+            ctx.fill();
+        }
+    }
+
+    function drawRightSpikes() {
+        for (var i = 0; i < rightSpikes.length; i++) {
+            var ls = rightSpikes[i];
+            ctx.beginPath();
+            ctx.moveTo(ls.x, ls.y);
+            ctx.lineTo(ls.tipX, ls.tipY);
+            ctx.lineTo(ls.x, ls.endY);
+            ctx.closePath();
+            ctx.fillStyle='#BE2116';
+            ctx.stroke();
+            ctx.fill();
+        }
+    }
+
 }
 
 window.addEventListener('load', function(event) {
